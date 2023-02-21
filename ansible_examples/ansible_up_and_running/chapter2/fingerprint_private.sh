f9675d9b234e0e74c98ed366114188cfbcbfea22
edtasixm01 <edtasixm01@gmail.com>
2023-02-21 18:24:26 +0100

    m06 vagrant22

---
+++ ansible_examples/ansible_up_and_running/chapter2/fingerprint_private.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+ssh-keyscan 172.30.30.10 >> ~/.ssh/known_hosts
+ssh-keyscan 172.30.30.11 >> ~/.ssh/known_hosts
+ssh-keyscan 172.30.30.12 >> ~/.ssh/known_hosts
+ssh-keyscan 172.30.30.13 >> ~/.ssh/known_hosts
+
